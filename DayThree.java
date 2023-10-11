e61e7dbca509e3c8ac5f47d20c21f806213f72c7
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-11 19:11:49 +0800

    DayThree
    Program tentang aritmatika

---
+++ DayThree.java
@@ -0,0 +1,27 @@
+package Days;
+
+public class DayThree {
+
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 3;
+        int hasil = a + b;
+        System.out.println("Hasil penambahan    : " + hasil);
+        
+        int c = 5;
+        int d = 20;
+        int hasill = a - b;
+        System.out.println("Hasil pengurangan   : " + hasill);
+        
+        int e = 9;
+        int f = 3;
+        int hasilll = a * b;
+        System.out.println("Hasil perkalian     : " + hasilll);
+        
+        int g = 10;
+        int h = 2;
+        int hasillll = a / b;
+        System.out.println("Hasil pembagian     : " + hasillll);
+
+    }
+}
